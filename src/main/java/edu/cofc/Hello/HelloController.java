c6aaf9723af6ce944dd18f3a1ed64eda0cacabc8
pabaier <pabaier@gmail.com>
2017-12-22 21:44:05 -0500

    data jpa and hibernate not working

--- src/main/java/edu/cofc/Hello/HelloController.java
+++ src/main/java/edu/cofc/Hello/HelloController.java
@@ -1,4 +1,4 @@
-package edu.cofc.hello;
+package edu.cofc.Hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/edu/cofc/topic/TopicController.java
+++ src/main/java/edu/cofc/topic/TopicController.java
@@ -2,9 +2,9 @@ package edu.cofc.topic;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
@@ -33,5 +33,17 @@ public class TopicController {
     public void addTopic(@RequestBody Topic topic) {
         topicService.addTopic(topic);
     }
+
+    @RequestMapping(method=RequestMethod.PUT, value="topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicService.updateTopic(topic, id);
+    }
+
+    @RequestMapping(method=RequestMethod.DELETE, value="topics/{id}")
+    public void deleteTopic(@PathVariable String id) {
+        topicService.deleteTopic(id);
+    }
+
+
 
 }
